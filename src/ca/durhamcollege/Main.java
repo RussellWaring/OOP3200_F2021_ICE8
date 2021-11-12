4925f3b4fb085def6048a2aec07abaf247d49548
RussellWaring <russell.waring@dcmail.ca>
2021-11-12 15:16:09 -0500

    Added an example about multi-dimensional arrays

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -5,6 +5,7 @@
  */
 package ca.durhamcollege;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -20,8 +21,56 @@ public class Main {
         name = keyboard.nextLine();
 
         float age = 0.0f;
-        System.out.print("Please enter your age: ");
-        age = keyboard.nextFloat();
+
+        boolean isValidInput = false; // Sentinel variable - used to check condition of while loop. Could also use a do...while loop
+        do
+        {
+            System.out.print("Please enter your age: ");
+            try
+            {
+                age = keyboard.nextFloat();
+                isValidInput = true;
+
+                if(age <= 0.0f || age > 120.0)
+                {
+                    System.out.println("Error: You must enter an age greater than 0.0 and less than 120.0");
+                    keyboard.nextLine();
+                    isValidInput = false;
+                }
+            }
+            catch (InputMismatchException inputMismatchException)
+            {
+                System.out.println("Error: You must enter a valid floating-point number");
+                keyboard.nextLine();
+                isValidInput = false;
+            }
+        }
+        while(!isValidInput);
+
+        // Multi-dimensional array example
+        System.out.println("Multi-dimensional Array example");
+        int rows = 8;
+        int cols = 8;
+        float[][] board = new float[rows][cols];
+
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < cols; y++)
+            {
+                board[x][y] = (float)(Math.random() * 100.0f) + 1.0f; // casting to a float, "old C-style"
+
+            }
+        }
+
+        System.out.println("Showing 10 Random Values:");
+        for (int i = 0; i < 10; i++)
+        {
+            var randRows = (int) (Math.random() * rows);// Math.floor rounds down
+            var randCols = (int) (Math.random() * cols);
+            System.out.print("For row: " + randRows + " col: " + randCols + " ");
+            System.out.printf("The Random number is: %.2f \n", board[randRows][randCols]);
+        }
+
 
         System.out.println(); // space
 
@@ -29,4 +78,5 @@ public class Main {
         System.out.printf("You Entered: %s\n", name);
         System.out.printf("You Entered: %.2f\n", age);
     }
+
 }
