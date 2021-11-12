a411b66ee927fd01b6ed91f55f0ab5cda6224736
RussellWaring <russell.waring@dcmail.ca>
2021-11-12 11:38:54 -0500

    Included basic input and output tests

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,20 @@
+/**
+ * @author  Russell Waring - 100528426
+ * @file    OOP3200 - In-class exercise 8
+ * @date    November 11th, 2021
+ */
 package ca.durhamcollege;
+
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        String name = null;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You Entered: %s", name);
     }
 }
